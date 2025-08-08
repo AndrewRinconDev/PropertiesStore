0e9238c5cf4746facdef6c5fe7bc80a646eb5b2b
Andres Rincon <andres.rincon@antechdx.com>
2025-08-08 14:29:08 -0500

    Build application layer

---
+++ PropertiesStored.Application/DTOs/PropertyListDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PropertiesStored.Application.DTOs
+{
+    public class PropertyListDto
+    {
+        public IEnumerable<PropertyDto> Properties { get; set; }
+        public PaginationDto Pagination { get; set; }
+    }
+}
