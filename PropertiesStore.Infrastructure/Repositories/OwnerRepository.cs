15d5d824df84b41c0d9c38fc84c4c4b3ea990a45
Andres Rincon <andres.rincon@antechdx.com>
2025-08-08 14:33:55 -0500

    remove unnecessaries imports

--- PropertiesStore.Infrastructure/Repositories/OwnerRepository.cs
+++ PropertiesStore.Infrastructure/Repositories/OwnerRepository.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using MongoDB.Driver;
+﻿using MongoDB.Driver;
 using PropertiesStore.Core.Entities;
 using PropertiesStore.Core.Interfaces;
 using PropertiesStore.Infrastructure.Data;
