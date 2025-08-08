3db5c9b3e9182610f965fc0f1c9b59ab0c2cb4d3
Andres Rincon <andres.rincon@antechdx.com>
2025-08-08 14:36:16 -0500

    Remove unnecessaries import

--- PropertiesStored.Application/DTOs/PropertyListDto.cs
+++ PropertiesStored.Application/DTOs/PropertyListDto.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace PropertiesStored.Application.DTOs
+﻿namespace PropertiesStored.Application.DTOs
 {
     public class PropertyListDto
     {
