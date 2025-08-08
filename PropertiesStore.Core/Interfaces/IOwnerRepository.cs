413c0c15c4f9ed70655cf85f9f465246874e5ec3
Andres Rincon <andres.rincon@antechdx.com>
2025-08-08 14:26:48 -0500

    Build core layer

---
+++ PropertiesStore.Core/Interfaces/IOwnerRepository.cs
@@ -0,0 +1,9 @@
+﻿using PropertiesStore.Core.Entities;
+
+namespace PropertiesStore.Core.Interfaces
+{
+    public interface IOwnerRepository
+    {
+        Task<Owner> GetOwnerByOwnerIdAsync(string ownerId);
+    }
+}
---
+++ PropertiesStore.Core/Interfaces/IPropertyRepository.cs
@@ -0,0 +1,20 @@
+﻿using PropertiesStore.Core.Entities;
+
+namespace PropertiesStore.Core.Interfaces
+{
+    public interface IPropertyRepository
+    {
+        Task<(IEnumerable<Property>, int)> GetPropertiesAsync(int page, int pageSize);
+        Task<(IEnumerable<Property>, int)> GetFilteredPropertiesAsync(
+            string name,
+            string address,
+            decimal? minPrice,
+            decimal? maxPrice,
+            int page,
+            int pageSize);
+        Task<Property> GetPropertyByIdAsync(string id);
+        Task AddPropertyAsync(Property property);
+        Task UpdatePropertyAsync(Property property);
+        Task DeletePropertyAsync(string id);
+    }
+}
